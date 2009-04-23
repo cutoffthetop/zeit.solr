7b82330b8644b7eda425aaa224e2c63318641e9d
hoshi <devnull@localhost>
2009-04-23 09:34:21 +0000

    Transform article's dav props into solr xml.

--- src/zeit/solr/converter.py
+++ src/zeit/solr/converter.py
@@ -1,3 +1,5 @@
+import lxml.objectify
+import lxml.etree
 import zeit.connector.interfaces
 import zeit.content.article
 import zeit.solr.interfaces
@@ -17,6 +19,18 @@ class SolrConverter(object):
     def prepare_dav_props(self):
         properties = zeit.connector.interfaces.IWebDAVProperties(self.context)
 
+        root_node = lxml.objectify.E.add()
+        doc_node = lxml.objectify.E.doc()
+        root_node.append(doc_node)
+
+        for prop_name, prop_ns in dict(properties):
+            field_node = lxml.objectify.E.field(
+                properties[prop_name, prop_ns],
+                name=prop_name
+            )
+            doc_node.append(field_node)
+
+        return root_node
 
     zope.component.adapts(zeit.content.article.interfaces.IArticle)
     zope.interface.implements(zeit.solr.interfaces.ISolrConverter)
