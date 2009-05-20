1b1c6dcaaf0c1c037073d6cbe2d92123352ce88f
hoshi <devnull@localhost>
2009-05-20 10:43:20 +0000

    Added new field authors_fulltext replacing authors, which is now an array. Extended filter functions to deal with array fields. Changed solr map into a list of quadruples.

--- src/zeit/solr/converter.py
+++ src/zeit/solr/converter.py
@@ -8,25 +8,42 @@ import zope.component
 import zope.interface
 
 
-def identity(arg):
-    return arg
+def identity(arg, solr_name, node):
+    field_node = lxml.objectify.E.field(
+        arg, name=solr_name
+    )
+    node.append(field_node)
 
-def join_tuple(tuple):
-    if tuple:
-        return ' '.join(unicode(arg) for arg in tuple)
+def join_tuple(tuple, solr_name, node):
+    append_to_node(
+        ' '.join(unicode(arg) for arg in tuple),
+        solr_name,
+        node
+    )
 
-def join_labels(tuple):
-    if tuple:
-        return ' '.join(unicode(arg.label) for arg in tuple)
+def split_tuple(tuple, solr_name, node):
+    for arg in tuple:
+        append_to_node(unicode(arg), solr_name, node)
 
-def canonize_date(arg):
-    if arg:
-        solr_date = str(arg).replace(' ','T', 1)
-        return solr_date.replace('+00:00', 'Z')
+def join_labels(tuple, solr_name, node):
+    append_to_node(
+        ' '.join(unicode(arg.label) for arg in tuple),
+        solr_name,
+        node
+    )
 
+def canonize_date(arg, solr_name, node):
+    solr_date = str(arg).replace(' ','T', 1)
+    solr_date = solr_date.replace('+00:00', 'Z')
+    append_to_node(unicode(solr_date), solr_name, node)
 
-def get_type(properties):
-    return properties.get(zeit.connector.interfaces.RESOURCE_TYPE_PROPERTY)
+def get_type(properties, solr_name, node):
+    type = properties.get(zeit.connector.interfaces.RESOURCE_TYPE_PROPERTY)
+    append_to_node(type, solr_name, node)
+
+def append_to_node(value, solr_name, parent_node):
+    child_node = lxml.objectify.E.field(value, name=solr_name)
+    parent_node.append(child_node)
 
 
 class SolrConverter(object):
@@ -38,82 +55,182 @@ class SolrConverter(object):
     zope.component.adapts(zeit.cms.interfaces.ICMSContent)
     zope.interface.implements(zeit.solr.interfaces.ISolrConverter)
 
-    solr_map = {
-        (zeit.cms.content.interfaces.ICommonMetadata, "authors")
-            : ("authors", join_tuple),
-        (zeit.cms.content.interfaces.ICommonMetadata, "byline")
-            : ("byline", identity),
-        (zeit.cms.workflow.interfaces.IModified, "date_last_modified")
-            : ("date-last-modified", canonize_date),
-        (zeit.cms.content.interfaces.ICommonMetadata, "shortTeaserText")
-            : ("indexteaser_title", identity),
-        (zeit.cms.content.interfaces.ICommonMetadata, "shortTeaserTitle")
-            : ("indexteaser_text", identity),
-        (zeit.cms.content.interfaces.ICommonMetadata, "keywords")
-            : ("keywords", join_labels),
-        (zeit.cms.workflow.interfaces.IModified, "last_modified_by")
-            : ("last_modified_by", canonize_date),
-        (zeit.cms.content.interfaces.ISemanticChange, "last_semantic_change")
-            : ("last-semantic-change", canonize_date),
-        (zeit.solr.interfaces.ISearchableText, "text")
-            : ("main_text", identity),
-        (zeit.cms.content.interfaces.ICommonMetadata, "boxMostRead")
-            : ("mostread", identity),
-        (zeit.cms.content.interfaces.ICommonMetadata, "page")
-            : ("page", identity),
-        (zeit.cms.workflow.interfaces.IPublishInfo, "published")
-            : ("published", identity),
-        (zeit.workflow.interfaces.IContentWorkflow, "refined")
-            : ("refined", identity),
-        (zeit.cms.content.interfaces.ICommonMetadata, "ressort")
-            : ("ressort", identity),
-        (zeit.cms.content.interfaces.ICommonMetadata, "serie")
-            : ("serie", identity),
-        (zeit.cms.content.interfaces.ICommonMetadata, "sub_ressort")
-            : ("sub_ressort", identity),
-        (zeit.cms.content.interfaces.ICommonMetadata, "subtitle")
-            : ("subtitle", identity),
-        (zeit.cms.content.interfaces.ICommonMetadata, "supertitle")
-            : ("supertitle", identity),
-        (zeit.cms.content.interfaces.ICommonMetadata, "teaserTitle")
-            : ("teaser_title", identity),
-        (zeit.cms.content.interfaces.ICommonMetadata, "teaserText")
-            : ("teaser_text", identity),
-        (zeit.cms.content.interfaces.ICommonMetadata, "title")
-            : ("title", identity),
-        (zeit.connector.interfaces.IWebDAVProperties, None)
-            : ("type", get_type),
-        (zeit.cms.content.interfaces.IUUID, "id")
-            : ("uuid", identity),
-        (zeit.cms.content.interfaces.ICommonMetadata, "volume")
-            : ("volume", identity),
-        (zeit.cms.interfaces.ICMSContent, "uniqueId")
-            : ("uniqueId", identity),
-    }
+    # This map contains the following types:
+    # interface, python attribute, solr attriute, filter function
+    solr_mapping = [
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'authors',
+            'authors',
+            split_tuple,
+        ),
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'authors',
+            'authors_fulltext',
+            join_tuple,
+        ),
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'byline',
+            'byline',
+            identity,
+        ),
+        (
+            zeit.cms.workflow.interfaces.IModified,
+            'date_last_modified',
+            'date-last-modified',
+            canonize_date,
+        ),
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'shortTeaserTitle',
+            'indexteaser_title',
+            identity,
+        ),
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'shortTeaserText',
+            'indexteaser_text',
+            identity,
+        ),
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'keywords',
+            'keywords',
+            join_labels,
+        ),
+        (
+            zeit.cms.workflow.interfaces.IModified,
+            'last_modified_by',
+            'last_modified_by',
+            canonize_date,
+        ),
+        (
+            zeit.cms.content.interfaces.ISemanticChange,
+            'last_semantic_change',
+            'last-semantic-change',
+            canonize_date,
+        ),
+        (
+            zeit.solr.interfaces.ISearchableText,
+            'text',
+            'main_text',
+            identity,
+        ),
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'boxMostRead',
+            'mostread',
+            identity,
+        ),
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'page',
+            'page',
+            identity,
+        ),
+        (
+            zeit.cms.workflow.interfaces.IPublishInfo,
+            'published',
+            'published',
+            identity,
+        ),
+        (
+            zeit.workflow.interfaces.IContentWorkflow,
+            'refined',
+            'refined',
+            identity,
+        ),
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'ressort',
+            'ressort',
+            identity,
+        ),
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'serie',
+            'serie',
+            identity,
+        ),
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'sub_ressort',
+            'sub_ressort',
+            identity,
+        ),
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'subtitle',
+            'subtitle',
+            identity,
+        ),
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'supertitle',
+            'supertitle',
+            identity,
+        ),
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'teaserTitle',
+            'teaser_title',
+            identity,
+        ),
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'teaserText',
+            'teaser_text',
+            identity,
+        ),
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'title',
+            'title',
+            identity,
+        ),
+        (
+            zeit.connector.interfaces.IWebDAVProperties,
+            None,
+            'type',
+            get_type,
+        ),
+        (
+            zeit.cms.content.interfaces.IUUID,
+            'id',
+            'uuid',
+            identity,
+        ),
+        (
+            zeit.cms.content.interfaces.ICommonMetadata,
+            'volume',
+            'volume',
+            identity,
+        ),
+        (
+            zeit.cms.interfaces.ICMSContent,
+            'uniqueId',
+            'uniqueId',
+            identity,
+        ),
+    ]
 
     def __init__(self, context):
         self.context = context
         self.adapters = {}
 
     def prepare_dav_props(self):
-        # This map contains the following types:
-        # interface, python attribute, solr attriute, filter function
         root_node = lxml.objectify.E.add()
         doc_node = lxml.objectify.E.doc()
         root_node.append(doc_node)
-        for ((interface, att_name), (solr_name, prepare)) in (
-            self.solr_map.items()):
+        for (interface, att_name, solr_name, prepare) in self.solr_mapping:
             value = self.get_adapter(interface)
             if att_name is not None:
                 value = getattr(value, att_name, None)
-            value = prepare(value)
-            if att_name == 'uuid' and not value:
-                raise ValueError("Cannot index without UUID.")
             if not value:
                 continue
-            field_node = lxml.objectify.E.field(
-                value, name=solr_name)
-            doc_node.append(field_node)
+            prepare(value, solr_name, doc_node)
         return root_node
 
     def get_adapter(self, interface):
