9d2f0fbda441313012bb76a0ca7d8dafb0da84fc
Christian Zagrodnick <cz@gocept.com>
2009-06-04 08:27:18 +0000

    made mapping of solr indexes more declarative

--- src/zeit/solr/converter.py
+++ src/zeit/solr/converter.py
@@ -1,3 +1,4 @@
+import inspect
 import lxml.etree
 import lxml.objectify
 import zeit.connector.interfaces
@@ -9,47 +10,84 @@ import zope.interface
 import zope.publisher.browser
 
 
-def join_tuple(tuple, solr_name, node):
-    append_to_node(
-        ' '.join(unicode(arg) for arg in tuple),
-        solr_name,
-        node
-    )
+class Index(object):
 
-def split_tuple(tuple, solr_name, node):
-    for arg in tuple:
-        append_to_node(unicode(arg), solr_name, node)
+    def __init__(self, interface, attribute, solr=None, stackup=1):
+        self.interface = interface
+        self.attribute = attribute
+        if solr is None:
+            solr = attribute
+        self.solr = solr
+        solr_mapping = inspect.stack()[stackup][0].f_locals.setdefault(
+            'solr_mapping', [])
+        solr_mapping.append(self)
 
-def join_labels(tuple, solr_name, node):
-    append_to_node(
-        ' '.join(unicode(arg.label) for arg in tuple),
-        solr_name,
-        node
-    )
+    def process(self, value, doc_node):
+        self.append_to_node(value, doc_node)
 
-def canonize_date(arg, solr_name, node):
-    solr_date = str(arg).replace(' ','T', 1)
-    solr_date = solr_date.replace('+00:00', 'Z')
-    append_to_node(unicode(solr_date), solr_name, node)
-
-def get_type(properties, solr_name, node):
-    type = properties.get(zeit.connector.interfaces.RESOURCE_TYPE_PROPERTY)
-    append_to_node(type, solr_name, node)
-
-def append_to_node(value, solr_name, parent_node):
-    child_node = lxml.objectify.E.field(value, name=solr_name)
-    parent_node.append(child_node)
+    def append_to_node(self, value, parent_node):
+        child_node = lxml.objectify.E.field(value, name=self.solr)
+        parent_node.append(child_node)
 
 
-def get_icon(context, default):
-    request = zope.publisher.browser.TestRequest(
-        skin=zeit.cms.browser.interfaces.ICMSSkin)
-    icon = zope.component.queryMultiAdapter(
-        (context, request), name='zmi_icon')
-    if icon is None:
-        return default
-    path = icon.url().replace(request['SERVER_URL'], '')
-    return path
+class JoinTuple(Index):
+
+    def process(self, value, doc_node):
+        self.append_to_node(' '.join(unicode(arg) for arg in value), doc_node)
+
+
+class SplitTuple(Index):
+
+    def process(self, value, doc_node):
+        for arg in value:
+            self.append_to_node(unicode(arg), doc_node)
+
+
+class Date(Index):
+
+    def process(self, value, doc_node):
+        solr_date = str(value).replace(' ','T', 1)
+        solr_date = solr_date.replace('+00:00', 'Z')
+        self.append_to_node(unicode(solr_date), doc_node)
+
+
+class Keywords(Index):
+
+    def process(self, value, doc_node):
+        self.append_to_node(' '.join(unicode(arg.label) for arg in value),
+                            doc_node)
+
+
+class Type(Index):
+
+    interface = zeit.connector.interfaces.IWebDAVProperties
+    attribute = None
+
+    def __init__(self, solr):
+        super(Type, self).__init__(self.interface, self.attribute, solr, 2)
+
+    def process(self, value, doc_node):
+        type = value.get(zeit.connector.interfaces.RESOURCE_TYPE_PROPERTY)
+        self.append_to_node(type, doc_node)
+
+
+class Icon(Index):
+
+    interface = zope.interface.Interface
+    attribute = None
+
+    def __init__(self, solr):
+        super(Icon, self).__init__(self.interface, self.attribute, solr, 2)
+
+    def process(self, value, doc_node):
+        request = zope.publisher.browser.TestRequest(
+            skin=zeit.cms.browser.interfaces.ICMSSkin)
+        icon = zope.component.queryMultiAdapter(
+            (value, request), name='zmi_icon')
+        if icon is None:
+            return
+        path = icon.url().replace(request['SERVER_URL'], '')
+        self.append_to_node(path, doc_node)
 
 
 class SolrConverter(object):
@@ -61,173 +99,87 @@ class SolrConverter(object):
     zope.component.adapts(zeit.cms.interfaces.ICMSContent)
     zope.interface.implements(zeit.solr.interfaces.ISolrConverter)
 
-    # This map contains the following types:
-    # interface, python attribute, solr attribute, filter function
-    solr_mapping = [
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'authors',
-            'authors',
-            split_tuple,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'authors',
-            'authors_fulltext',
-            join_tuple,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'byline',
-            'byline',
-            append_to_node,
-        ),
-        (
-            zeit.cms.workflow.interfaces.IModified,
-            'date_last_modified',
-            'date-last-modified',
-            canonize_date,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'shortTeaserTitle',
-            'indexteaser_title',
-            append_to_node,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'shortTeaserText',
-            'indexteaser_text',
-            append_to_node,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'keywords',
-            'keywords',
-            join_labels,
-        ),
-        (
-            zeit.cms.workflow.interfaces.IModified,
-            'last_modified_by',
-            'last_modified_by',
-            canonize_date,
-        ),
-        (
-            zeit.cms.content.interfaces.ISemanticChange,
-            'last_semantic_change',
-            'last-semantic-change',
-            canonize_date,
-        ),
-        (
-            zeit.solr.interfaces.ISearchableText,
-            'text',
-            'main_text',
-            append_to_node,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'boxMostRead',
-            'mostread',
-            append_to_node,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'page',
-            'page',
-            append_to_node,
-        ),
-        (
-            zeit.cms.workflow.interfaces.IPublicationStatus,
-            'published',
-            'published',
-            append_to_node,
-        ),
-        (
-            zeit.workflow.interfaces.IContentWorkflow,
-            'refined',
-            'refined',
-            append_to_node,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'ressort',
-            'ressort',
-            append_to_node,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'serie',
-            'serie',
-            append_to_node,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'sub_ressort',
-            'sub_ressort',
-            append_to_node,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'subtitle',
-            'subtitle',
-            append_to_node,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'supertitle',
-            'supertitle',
-            append_to_node,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'teaserTitle',
-            'teaser_title',
-            append_to_node,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'teaserText',
-            'teaser_text',
-            append_to_node,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'title',
-            'title',
-            append_to_node,
-        ),
-        (
-            zeit.connector.interfaces.IWebDAVProperties,
-            None,
-            'type',
-            get_type,
-        ),
-        (
-            zeit.cms.content.interfaces.IUUID,
-            'id',
-            'uuid',
-            append_to_node,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'volume',
-            'volume',
-            append_to_node,
-        ),
-        (
-            zeit.cms.interfaces.ICMSContent,
-            'uniqueId',
-            'uniqueId',
-            append_to_node,
-        ),
-        (
-            zeit.cms.content.interfaces.ICommonMetadata,
-            'year',
-            'year',
-            append_to_node,
-        ),
-        (get_icon, None, 'icon', append_to_node),
-    ]
+    SplitTuple(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'authors')
+    JoinTuple(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'authors', solr='authors_fulltext')
+    Index(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'byline')
+    Date(zeit.cms.workflow.interfaces.IModified,
+         'date_last_modified', solr='date-last-modified')
+    Index(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'shortTeaserTitle',
+        solr='indexteaser_title')
+    Index(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'shortTeaserText',
+        solr='indexteaser_text')
+    Keywords(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'keywords')
+    Index(
+        zeit.cms.workflow.interfaces.IModified,
+        'last_modified_by')
+    Date(
+        zeit.cms.content.interfaces.ISemanticChange,
+        'last_semantic_change', solr='last-semantic-change')
+    Index(
+        zeit.solr.interfaces.ISearchableText,
+        'text', solr='main_text')
+    Index(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'boxMostRead', solr='mostread'),
+    Index(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'page')
+    Index(
+        zeit.cms.workflow.interfaces.IPublicationStatus,
+        'published')
+    Index(
+        zeit.workflow.interfaces.IContentWorkflow,
+        'refined')
+    Index(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'ressort')
+    Index(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'serie')
+    Index(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'sub_ressort')
+    Index(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'subtitle')
+    Index(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'supertitle')
+    Index(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'teaserTitle', solr='teaser_title')
+    Index(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'teaserText', solr='teaser_text')
+    Index(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'title')
+    Type(solr='type')
+    Index(
+        zeit.cms.content.interfaces.IUUID,
+        'id', solr='uuid')
+    Index(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'volume')
+    Index(
+        zeit.cms.interfaces.ICMSContent,
+        'uniqueId')
+    Index(
+        zeit.cms.content.interfaces.ICommonMetadata,
+        'year')
+    Icon(solr='icon')
 
     def __init__(self, context):
         self.context = context
@@ -237,13 +189,13 @@ class SolrConverter(object):
         root_node = lxml.objectify.E.add()
         doc_node = lxml.objectify.E.doc()
         root_node.append(doc_node)
-        for (interface, att_name, solr_name, prepare) in self.solr_mapping:
-            value = self.get_adapter(interface)
-            if att_name is not None:
-                value = getattr(value, att_name, None)
+        for index in self.solr_mapping:
+            value = self.get_adapter(index.interface)
+            if index.attribute is not None:
+                value = getattr(value, index.attribute, None)
             if not value:
                 continue
-            prepare(value, solr_name, doc_node)
+            index.process(value, doc_node)
         return root_node
 
     def get_adapter(self, interface):
