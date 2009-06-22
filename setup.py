656a99c478d5eb14514059308bd3aa7a38d31034
Wolfgang Schnerring <ws@gocept.com>
2009-06-22 07:52:28 +0000

    Re #5519: renamed worker.py to update.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     ],
     entry_points = """
         [console_scripts]
-        solr-updater = zeit.solr.worker:update_main
+        solr-updater = zeit.solr.update:update_main
         """
 
 )
