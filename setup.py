c77a40418655a95794a9db3dbf7de4f0febc5b70
Christian Zagrodnick <cz@gocept.com>
2010-04-09 14:47:20 +0000

    Preparing release 0.19.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'zeit.solr',
-    version = '0.18.2dev',
+    version = '0.19.0',
     author = 'Dominik Hoppe',
     author_email = 'dominik.hoppe@zeit.de',
     description = 'Get articles from the repository and prepare them for solr.',
