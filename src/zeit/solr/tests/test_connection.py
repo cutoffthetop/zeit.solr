6ead36c09aa8635cdf981b24dd2cf8d6644695dc
Christian Zagrodnick <cz@gocept.com>
2010-05-26 08:05:18 +0000

    re #7241: use HTTPServerLayer from zeit.cms.testing in zeit.solr instead of the old, hand-crafted way

--- src/zeit/solr/tests/test_connection.py
+++ src/zeit/solr/tests/test_connection.py
@@ -7,14 +7,16 @@ import pysolr
 import random
 import threading
 import unittest
+import zeit.cms.testing
 import zeit.solr.connection
 
 
-class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler, object):
+class RequestHandler(zeit.cms.testing.BaseHTTPRequestHandler):
 
     response_code = 200
     response_body = ''
     reason = None
+    posts_received = []
 
     def do_POST(self):
         length = int(self.headers['content-length'])
@@ -29,54 +31,42 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler, object):
         self.end_headers()
         self.wfile.write(self.response_body)
 
-    def log_message(self, format, *args):
-        # Silence logging
-        pass
+
+HTTPLayer, port = zeit.cms.testing.HTTPServerLayer(RequestHandler)
 
 
 class TestSolrConnection(unittest.TestCase):
 
+    layer = HTTPLayer
+
     def setUp(self):
         super(TestSolrConnection, self).setUp()
-        self.port = random.randint(30000, 40000)
         self.solr = zeit.solr.connection.SolrConnection(
-            'http://localhost:%s/solr/' % self.port)
-        self.RequestHandler = type('TestRequestHandler', (RequestHandler,),
-                                   {'posts_received': []})
+            'http://localhost:%s/solr/' % port)
         self.data = lxml.objectify.XML('<foo/>')
 
     def tearDown(self):
-         self.httpd.shutdown()
-         super(TestSolrConnection, self).tearDown()
-
-    def start_httpd(self):
-        self.thread = threading.Thread(target=self.start_httpd_handler)
-        self.thread.start()
-
-    def start_httpd_handler(self):
-        self.running = True
-        server_address = ('localhost', self.port)
-        self.httpd = BaseHTTPServer.HTTPServer(server_address,
-                                               self.RequestHandler)
-        self.httpd.serve_forever(0.1)
+        RequestHandler.posts_received[:] = []
+        RequestHandler.response_code = 200
+        RequestHandler.response_body = ''
+        RequestHandler.reason = None
+        super(TestSolrConnection, self).tearDown()
 
     def test_update_raw(self):
-        self.start_httpd()
         self.solr.update_raw(self.data)
-        self.assertEquals(1, len(self.RequestHandler.posts_received))
-        post = self.RequestHandler.posts_received
+        self.assertEquals(1, len(RequestHandler.posts_received))
+        post = RequestHandler.posts_received
         self.assertEquals('/solr/update/', post[0]['path'])
         self.assertEquals("<?xml version='1.0' encoding='UTF-8'?>\n<foo/>",
                           post[0]['data'])
         self.assertEquals('text/xml', post[0]['headers']['Content-Type'])
 
     def test_update_raw_with_error(self):
-        self.RequestHandler.response_code = 500
-        self.RequestHandler.response_body = (
+        RequestHandler.response_code = 500
+        RequestHandler.response_body = (
             "<html><body><h1>Failed for some reason</h1> bla"
             "<hr></body></html>")
-        self.RequestHandler.reason = 'Failed for a reason'
-        self.start_httpd()
+        RequestHandler.reason = 'Failed for a reason'
         try:
             self.solr.update_raw(self.data)
         except pysolr.SolrError, e:
@@ -86,12 +76,11 @@ class TestSolrConnection(unittest.TestCase):
                 e.args[0])
         else:
             self.fail("Exception not raised")
-        self.assertEquals(1, len(self.RequestHandler.posts_received))
+        self.assertEquals(1, len(RequestHandler.posts_received))
 
     def test_update_raw_with_error_and_strange_html(self):
-        self.RequestHandler.response_code = 500
-        self.RequestHandler.response_body = "this is not html & broken"
-        self.start_httpd()
+        RequestHandler.response_code = 500
+        RequestHandler.response_body = "this is not html & broken"
         try:
             self.solr.update_raw(self.data)
         except pysolr.SolrError, e:
@@ -100,13 +89,12 @@ class TestSolrConnection(unittest.TestCase):
                 e.args[0])
         else:
             self.fail("Exception not raised")
-        self.assertEquals(1, len(self.RequestHandler.posts_received))
+        self.assertEquals(1, len(RequestHandler.posts_received))
 
     def test_delete_escapes_xml(self):
-        self.start_httpd()
         self.solr.delete(q='foo:a\\&b', commit=False)
-        self.assertEquals(1, len(self.RequestHandler.posts_received))
-        post = self.RequestHandler.posts_received
+        self.assertEquals(1, len(RequestHandler.posts_received))
+        post = RequestHandler.posts_received
         self.assertEquals(
             '<delete><query>foo:a\\&amp;b</query></delete>',
             post[0]['data'])
