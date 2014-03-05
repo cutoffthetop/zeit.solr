091cf43af11df3cac8771ea08fa3ccf138447189
Wolfgang Schnerring <ws@gocept.com>
2014-03-05 11:29:41 +0100

    pep8

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'zeit.solr',
+    name='zeit.solr',
     version='2.1.2.dev0',
-    author = 'Dominik Hoppe',
-    author_email = 'dominik.hoppe@zeit.de',
-    description = 'Get articles from the repository and prepare them for solr.',
-    packages = find_packages('src'),
-    package_dir = {'' : 'src'},
-    include_package_data = True,
-    zip_safe = False,
-    namespace_packages = ['zeit'],
-    install_requires = [
+    author='Dominik Hoppe',
+    author_email='dominik.hoppe@zeit.de',
+    description='Get articles from the repository and prepare them for solr.',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    namespace_packages=['zeit'],
+    install_requires=[
         'gocept.async>=0.3.1',
         'grokcore.component',
         'httplib2',
@@ -25,9 +25,9 @@ setup(
         'zeit.content.portraitbox',
         'zope.index',
     ],
-    entry_points = """
+    entry_points="""
         [console_scripts]
-        solr-reindex-object = zeit.solr.update:update_main
-        solr-reindex-query = zeit.solr.reindex:reindex
+        solr-reindex-object=zeit.solr.update:update_main
+        solr-reindex-query=zeit.solr.reindex:reindex
         """
 )
